1651409e48c7ddb9f6cc8634778f8a3444c01c27
agrawal-ankush <ankush.agrawal@datasemantics.co>
2024-05-11 14:39:23 +0530

    Using Pydantic lib to validate incomming data. Following best practice for defining routes name (always use plurel names)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    # defining two optional fields one with out using library and one using Optional from typing library
+    published: bool = True
+    rating: Optional[int] = None
+    
+
 
 
 @app.get("/")
@@ -14,7 +26,14 @@ def get_posts():
     return {"data": "this is your posts"}
 
 
-@app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new post ": f"title: {payload['title']} content: {payload['content']} "}
+@app.post("/posts")
+# def create_post(payload: dict = Body(...)):
+# lets use Pydantic here. Arguments new_post is refrence and Post is model name   
+def create_post(new_post: Post):
+    print(new_post)
+
+    # we can also convert pydantic model to dictionary
+    print(new_post.model_dump())
+          
+
+    return {"data ": f"{new_post}"}
