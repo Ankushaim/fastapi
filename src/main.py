1805334a57b9904f03f44eb535d91602fbb2e976
agrawal-ankush <ankush.agrawal@datasemantics.co>
2024-05-11 13:07:07 +0530

    Added few more routes to main file with methods like get and post. Added created post route to accept data from API body. For this we need to import Body from fastapi.prams.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -8,6 +9,12 @@ async def root():
     return {"message": "Hello Worlds"}
 
 
-@app.get('/test')
-async def root():
-    return {'key': 'value'}
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your posts"}
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new post ": f"title: {payload['title']} content: {payload['content']} "}
