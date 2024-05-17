ef426fde683382e3d468180842b885ecb951fb53
agrawal-ankush <ankush.agrawal@datasemantics.co>
2024-05-17 11:23:11 +0530

    Moved to ORM based sqlalchemy from Raw sql. database.py contains code to connect with sqlalchemy. models.py contains db models which are equal to our db table. Commented old code of raw sql and started using sqlalchemy based ORM models to query db.

--- src/app/main.py
+++ src/app/main.py
@@ -1,24 +1,36 @@
-import time
-from typing import Optional
+from typing import Any
 
-import psycopg2
+from fastapi import Depends
 from fastapi import FastAPI, status, HTTPException
-from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+import models
+from database import engine, get_db
+
+"""below will create db tables if not exists in db when we start our project. If its already available it will create new tables.
+If there is any change we did in models we need to first delete table from the db then only new table will be created with the changes.
+"""
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="fastApiLearning", user="postgres", password="root",
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        print("Database connection established")
-        break
-    except Exception as e:
-        print("Failed to connect")
-        print(e)
-        time.sleep(2)
+
+# Dependency
+
+
+# This code only requires if we are using db driver only without sqlalchemy
+# while True:
+#     try:
+#         conn = psycopg2.connect(host="localhost", database="fastApiLearning", user="postgres", password="root",
+#                                 cursor_factory=RealDictCursor)
+#         cursor = conn.cursor(cursor_factory=RealDictCursor)
+#         print("Database connection established")
+#         break
+#     except Exception as e:
+#         print("Failed to connect")
+#         print(e)
+#         time.sleep(2)
 
 
 class Post(BaseModel):
@@ -26,7 +38,6 @@ class Post(BaseModel):
     content: str
     # optional fields below
     published: bool = True
-    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -35,50 +46,80 @@ async def root():
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("SELECT * FROM post")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("SELECT * FROM post")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(new_post: Post) -> dict[str, str]:
-    cursor.execute("""INSERT INTO post (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                   (new_post.title, new_post.content, new_post.published))
-    new_created_post = cursor.fetchone()
-    conn.commit()
-    return {"data ": f"{new_created_post}"}
+def create_post(new_post: Post, db: Session = Depends(get_db)) -> dict[str, Any]:
+    # cursor.execute("""INSERT INTO post (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #                (new_post.title, new_post.content, new_post.published))
+    # new_created_post = cursor.fetchone()
+    # conn.commit()
+
+    # the below step is time consuming is we have 50 fields in db table instead this we can follow other approach
+    # new_post_query = models.Post(title=new_post.title, content=new_post.content, published = new_post.published)
+
+    new_post_dict = new_post.model_dump()  # converting payload to dict
+    new_post_query = models.Post(**new_post_dict)  # unpacking dict(keys and values in query)
+    db.add(new_post_query)
+    db.commit()
+    db.refresh(new_post_query)
+
+    # return {"data ": new_post_query}  //this is throwing an exception
+    return {"message": {
+        "id": new_post_query.id,
+        "title": new_post_query.title,
+        "content": new_post_query.content,
+        "published": new_post_query.published,
+        "created_at": new_post_query.created_at
+    }}
 
 
+# TODO: This routes needs to be updated with ORM changes
 @app.get("/posts/{id}")
-def get_posts(id: int):
-    cursor.execute("""SELECT * FROM post WHERE id = %s """, (str(id),))
-    post = cursor.fetchone()
+def get_posts(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM post WHERE id = %s """, (str(id),))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).all()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"message": f"Post with id {id} not found"})
     return {"post_details": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM post WHERE id = %s RETURNING *""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post is None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM post WHERE id = %s RETURNING *""", (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    if post_query.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail={"message": f"No post can be found with this {id}. Please provide valid post id"})
+    post_query.delete(synchronize_session=False)
+    db.commit()
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_post(id: int, new_post: Post):
-    cursor.execute(""" UPDATE post SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                   (new_post.title, new_post.content, new_post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post is None:
+def update_post(id: int, new_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(""" UPDATE post SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+    #                (new_post.title, new_post.content, new_post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    if post_query.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail={"message": f"No post can be found with this {id}. Please provide valid post id"})
     else:
-        return {"updated_post": updated_post}
-
-# Test commit
+        post_dict = new_post.model_dump()
+        post_query.update({
+            **post_dict
+        }, synchronize_session=False)
+        db.commit()
+        return {"status": "success"}
---
+++ src/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+
+from database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False, )
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='True', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
