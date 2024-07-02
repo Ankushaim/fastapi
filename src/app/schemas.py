27914c90b58206b9936a185b17d060169e860b6b
agrawal-ankush <ankush.agrawal@datasemantics.co>
2024-07-03 00:38:49 +0530

    final commit

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel, EmailStr
 
@@ -48,3 +49,11 @@ class UserLogin(BaseModel):
 
     class Config:
         extra = "forbid"
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
